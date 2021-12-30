e4f85763f6c4f218a02def890fd262189d6d0ef7
nhattran134 <nhattran134@gmail.com>
2021-12-30 17:21:11 +0700

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,4 +1,8 @@
-common_tags = merge(
-    var.tags, {
-      "Project" = var.project
-  })
+locals {
+  account_id = data.aws_caller_identity.current.account_id
+  region     = data.aws_region.current.name
+    common_tags = merge(
+        var.tags, {
+        "Project" = var.project
+    })
+}
